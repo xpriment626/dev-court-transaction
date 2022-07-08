b1d8996c3f825d7299e59025f66e64a6b91aa110
Emmett Childs <emmettchilds@thegobospot.com>
2022-07-08 13:41:39 +0800

    encoded function data

--- transaction.js
+++ transaction.js
@@ -1,56 +1,42 @@
 const ethers = require("ethers");
 const fs = require("fs-extra");
 require("dotenv").config();
-import { devCourt, solCourt, jsCourt } from "./court-params";
+import { Interface } from "ethers/lib/utils";
+// import { devCourt, solCourt, jsCourt } from "./court-params";
 
 const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
 const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
-const abi = fs.readFileSync("./xKlerosLiquid_sol_xKlerosLiquid.abi", "utf8");
+// const abi = fs.readFileSync("./xKlerosLiquid_sol_xKlerosLiquid.abi", "utf8");
 const contractAddress = "0x87E1bfEB31Ac4FA857a08471847122ec338F3cF2";
+const gasPrice = provider.getGasPrice();
+const signer = wallet.connect(provider);
 
-const contract = new ethers.Contract(contractAddress, abi, provider);
+const iface = new Interface([
+    "function createSubcourt(uint96 parent, bool hiddenVotes, uint minStake, uint alpha, uint feeForJuror, uint jurorsForCourtJump, uint[4] timesPerPeriod, uint sortitionSumTreeK)",
+]);
+const txData = iface.encodeFunctionData("createSubcourt", [
+    "0",
+    "false",
+    "30",
+    "1400",
+    "0.5",
+    "63",
+    "[280800, 437400, 437400, 291600]",
+    "0",
+]);
 
 async function main() {
-    const contractWalletConnection = contract.connect(wallet);
+    const tx = {
+        from: wallet.address,
+        to: contractAddress,
+        value: 0,
+        gasPrice: gasPrice,
+        gasLimit: ethers.utils.hexlify(200000),
+        nonce: provider.getTransactionCount(wallet.address),
+        data: txData,
+    };
 
-    const xDaiDevCourt = await contractWalletConnection.createSubcourt(
-        devCourt.parent,
-        devCourt.hiddenVotes,
-        devCourt.minStake,
-        devCourt.alpha,
-        devCourt.feeForJuror,
-        devCourt.jurorsForCourtJump,
-        devCourt.timesPerPeriod,
-        devCourt.sortitionSumTreeK
-    );
-
-    await xDaiDevCourt.wait(1);
-
-    const xDaiSolCourt = await contractWalletConnection.createSubcourt(
-        solCourt.parent,
-        solCourt.hiddenVotes,
-        solCourt.minStake,
-        solCourt.alpha,
-        solCourt.feeForJuror,
-        solCourt.jurorsForCourtJump,
-        solCourt.timesPerPeriod,
-        solCourt.sortitionSumTreeK
-    );
-
-    await xDaiSolCourt.wait(1);
-
-    const xDaiJSCourt = await contractWalletConnection.createSubcourt(
-        jsCourt.parent,
-        jsCourt.hiddenVotes,
-        jsCourt.minStake,
-        jsCourt.alpha,
-        jsCourt.feeForJuror,
-        jsCourt.jurorsForCourtJump,
-        jsCourt.timesPerPeriod,
-        jsCourt.sortitionSumTreeK
-    );
-
-    await xDaiJSCourt.wait(1);
+    await signer.sendTransaction(tx);
 }
 
 main()
